5c74185e644a990ea39fb6ea87adcd9ca097ae4c
Chen Yang <yangc5@mail.uc.edu>
2016-06-05 16:01:44 -0500

    added client

---
+++ client/app/scripts/app.js
@@ -0,0 +1,89 @@
+'use strict';
+/**
+ * @ngdoc overview
+ * @name sbAdminApp
+ * @description
+ * # sbAdminApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('sbAdminApp', [
+    'oc.lazyLoad',
+    'ui.router',
+    'ui.bootstrap',
+    'angular-loading-bar',
+  ])
+  .config(['$stateProvider','$urlRouterProvider','$ocLazyLoadProvider',function ($stateProvider,$urlRouterProvider,$ocLazyLoadProvider) {
+
+    $ocLazyLoadProvider.config({
+      debug:false,
+      events:true,
+    });
+
+    $urlRouterProvider.otherwise('/dashboard');
+
+    $stateProvider
+      .state('dashboard', {
+        url:'/dashboard',
+        templateUrl: 'views/dashboard/main.html',
+        resolve: {
+            loadMyDirectives:function($ocLazyLoad){
+                return $ocLazyLoad.load(
+                {
+                    name:'sbAdminApp',
+                    files:[
+                    'scripts/directives/header/header.js',
+                    'scripts/directives/header/header-notification/header-notification.js',
+                    'scripts/directives/sidebar/sidebar.js',
+                    'scripts/directives/sidebar/sidebar-search/sidebar-search.js'
+                    ]
+                }),
+                $ocLazyLoad.load(
+                {
+                   name:'toggle-switch',
+                   files:["bower_components/angular-toggle-switch/angular-toggle-switch.min.js",
+                          "bower_components/angular-toggle-switch/angular-toggle-switch.css"
+                      ]
+                }),
+                $ocLazyLoad.load(
+                {
+                  name:'ngAnimate',
+                  files:['bower_components/angular-animate/angular-animate.js']
+                })
+                $ocLazyLoad.load(
+                {
+                  name:'ngCookies',
+                  files:['bower_components/angular-cookies/angular-cookies.js']
+                })
+                $ocLazyLoad.load(
+                {
+                  name:'ngResource',
+                  files:['bower_components/angular-resource/angular-resource.js']
+                })
+                $ocLazyLoad.load(
+                {
+                  name:'ngSanitize',
+                  files:['bower_components/angular-sanitize/angular-sanitize.js']
+                })
+                $ocLazyLoad.load(
+                {
+                  name:'ngTouch',
+                  files:['bower_components/angular-touch/angular-touch.js']
+                })
+            }
+        }
+    })
+      .state('dashboard.blank',{
+        templateUrl:'views/pages/blank.html',
+        url:'/blank'
+    })
+      .state('login',{
+        templateUrl:'views/pages/login.html',
+        url:'/login'
+    })
+      .state('dashboard.notifications',{
+        templateUrl:'views/ui-elements/notifications.html',
+        url:'/notifications'
+    })
+  }]);
